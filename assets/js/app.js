e256d5e212a2b5fcff5e9fb8e6d0b44d14a08c6e
Joe Terlecki <joe.p.terlecki@gmail.com>
2022-04-03 00:09:43 -0400

    fixed switch for python

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,37 @@
 function getString() {
   document.getElementById("alert").classList.add("invisible");
   let normalString = document.getElementById("normalString").value;
-  let switchedString = reverseString(normalString);
 
-  dispalyString(switchedString);
 
-}
-
-function reverseString(normalString) {
-  let switchedString = [];
-
-  for (let character = normalString.length - 1; character >= 0; character--) {
-    switchedString += normalString[character];
-  }
-
-  return switchedString;
+  dispalyString(normalString);
 
 }
 
-function dispalyString(switchedString) {
+function dispalyString(normalString) {
+  // const url = `https://api.joeterlecki.io/0-to-100?start_number=${startNumber}&end_number=${endNumber}`
+  const url = `https://api.joeterlecki.io/switched?request_string=${normalString}`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // let numbers = data["number_list"];
+      // let templateRows = "";
+      // for (let number = 0; number < numbers.length; number++) {
+      //     let displayNumber = numbers[number];
+      //     if (displayNumber % 2 === 0) {
+      //         templateRows += `<tr><td class="align-middle text-center"><span class="text-primary text-gradient text-xs font-weight-bold">${displayNumber}</span></td></tr>`;
+      //     } else {
+      //         templateRows += `<tr><td class="align-middle text-center"><span class="text-secondary text-xs">${displayNumber}</span></td></tr>`;
+      //     }
+      // }
+      // document.getElementById("results").innerHTML = templateRows;
+      document.getElementById("message").innerHTML = `${data["response"]}`;
 
-  document.getElementById("message").innerHTML = `${switchedString}`;
+      document.getElementById("alert").classList.remove("invisible");
+    })
 
-  document.getElementById("alert").classList.remove("invisible");
+  // document.getElementById("message").innerHTML = `${switchedString}`;
+
+  // document.getElementById("alert").classList.remove("invisible");
 }

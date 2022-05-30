ab951de5116884c95ee5fa44a211ae26a1a17806
Cloud Custodian <joe.p.terlecki@gmail.com>
2022-05-30 11:15:52 -0400

    learning

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,60 @@
-function getString() {
-  document.getElementById("alert").classList.add("invisible");
-  let normalString = document.getElementById("normalString").value;
+const StringController = (function () {
+  return {
+    convertString: function (normalString) {
+      let newString = [];
+      for (let char = normalString.length - 1; char >= 0; char -- ) {
+        newString += normalString[char];
+      }
+      return newString;
+    }
+  }
+})();
+
+const UIController = (function () {
+  const uiSelectors = {
+    submitButton: '#btnSubmit',
+    userString: '#normalString',
+    convertedString: '#message',
+    alertBox: '#alert',
+  }
+
+  return {
+    getSelectors: function () {
+      return uiSelectors;
+    },
+
+    getStringInput: function () {
+      return {
+        normalString: document.querySelector(uiSelectors.userString).value
+      }
+    },
+
+    displayNewString: function (newString) {
+      document.querySelector(uiSelectors.convertedString).innerHTML = `${newString}`;
+      document.querySelector(uiSelectors.alertBox).classList.remove("invisible");
+    }
+  }
+
+})();
 
 
-  dispalyString(normalString);
+const AppController = (function (StringController, UIController) {
+  const loadEventListeners = function () {
+    const uiSelectors = UIController.getSelectors();
+    document.querySelector(uiSelectors.submitButton).addEventListener('click', submitString);
+  }
 
-}
+  const submitString = function (event) {
+    const input = UIController.getStringInput();
+    const convertedString = StringController.convertString(input.normalString);
+    UIController.displayNewString(convertedString);
+    event.preventDefault();
+  }
+  return {
+    init: function () {
+      loadEventListeners();
+    }
+  }
+})(StringController, UIController);
 
-function dispalyString(normalString) {
-  const url = `https://api.joeterlecki.io/switched?request_string=${normalString}`;
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      document.getElementById("message").innerHTML = `${data["response"]}`;
-
-      document.getElementById("alert").classList.remove("invisible");
-    })
-}
+AppController.init();
